eef2935606e158c10938c6f65236e88d4a217999
mansoorulhaq324 <42800922+mansoorulhaq324@users.noreply.github.com>
2024-06-28 20:19:09 +0500

    for demo

---
+++ AuthDemo.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using AuthDemo.Application.Abstractions.Application;
+using AuthDemo.Application.Services;
+using AuthDemo.Domain.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthDemo.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IUserResolverService, UserResolverService>();
+
+        services.Configure<DatabaseConfiguration>(configuration.GetSection("DatabaseConfiguration"));
+        services.Configure<AuthenticationConfiguration>(configuration.GetSection("AuthenticationConfiguration"));
+        services.Configure<HashingConfiguration>(configuration.GetSection("HashingConfiguration"));
+        return services;
+    }
+}
---
+++ AuthDemo.Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using AuthDemo.Application.Abstractions.Persistence;
+using AuthDemo.Persistence.Contexts;
+using AuthDemo.Persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthDemo.Persistence;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AuthDemoDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("InMemoryDbName"); // You can replace "InMemoryDbName" with your preferred database name.
+        });
+
+        services.AddScoped<IAuthRepository, AuthRepository>();
+
+        return services;
+    }
+}
